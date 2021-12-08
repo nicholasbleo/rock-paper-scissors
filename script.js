96f1ec321f2757259e297d6e9404707d3b243f11
Nicholas Leo <n.b.leo@protonmail.com>
2021-12-08 15:20:25 -0600

    Implemented UI

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+let gameCounter = 0;
+let winCounter = 0;
+let loseCounter = 0;
+let tieCounter = 0;
+
+
 function computerPlay() {
 
     let randomNumber = Math.random() * 100;
-    let computerHand;
+    let computerSelection;
 
     if (randomNumber <= 33) {
-        computerHand = 'rock';
+        computerSelection = 'rock';
     } else if (randomNumber <= 66) {
-        computerHand = 'paper';
+        computerSelection = 'paper';
     } else {
-        computerHand = 'scissors';
+        computerSelection = 'scissors';
     }
 
-    return computerHand;
-}
+    return computerSelection;
+};
 
-console.log(computerPlay);
+
 
 function playerPlay() {
 
@@ -33,70 +39,125 @@ function playerPlay() {
     }
 
     return playerSelection;
-}
+};
+
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissors' ||
         playerSelection === 'paper' && computerSelection === 'rock' ||
         playerSelection === 'scissors' && computerSelection === 'paper') {
-
-        return 'win'
+        
+        gameCounter += 1;
+        winCounter += 1;
+        return 'win';
 
     } else if (playerSelection === computerSelection) {
 
+        gameCounter += 1;
+        tieCounter += 1;
         return 'tie';
 
     } else {
 
+        gameCounter += 1;
+        loseCounter += 1;
         return 'lose';
         
-    }
-}
+    };
+};
 
-function game() {
-
-    let gameCounter = 0;
-    let winCounter = 0;
-    let loseCounter = 0;
-    let tieCounter = 0;
-
-    while (gameCounter < 5) {
-
-        let playerSelection = playerPlay();
-        let computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        gameCounter += 1;
-
-        if (result === 'win') {
-
-            console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n You win!');
-            winCounter += 1;
-
-        } else if (result === 'lose') {
-
-            console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n You lose!');
-            loseCounter += 1;
-
+function checkScore() {
+    if (gameCounter >= 5) {
+        if (winCounter > loseCounter) {
+            winnerDiv.innerText = 'You win!';
+        } else if (loseCounter > winCounter) {
+            winnerDiv.innerText = 'You lose!';
         } else {
-            console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n Try again!');
-            tieCounter += 1;
+            winnerDiv.innerText = 'It\'s a tie!';
         }
-
-    }
-
-    console.log('Wins: ' + winCounter);
-    console.log('Losses: ' + loseCounter);
-    console.log('Ties: ' + tieCounter)
-
-    if (winCounter > loseCounter) {
-        console.log('You win!')
-    } else if (winCounter < loseCounter) {
-        console.log('You Lose!')
-    } else {
-        console.log('It\'s a tie!')
-    }
-
+    };
 }
 
-game();
+const winsDiv = document.getElementById('wins');
+const lossDiv = document.getElementById('losses');
+const tiesDiv = document.getElementById('ties');
+const totalDiv = document.getElementById('total');
+const winnerDiv = document.getElementById('winner');
+
+
+const rockButton = document.getElementById('rock');
+rockButton.addEventListener('click', e => {
+    playRound(e.target.value, computerPlay());
+    winsDiv.innerText = `Wins: ${winCounter}`;
+    lossDiv.innerText = `Losses: ${loseCounter}`;
+    tiesDiv.innerText = `Ties: ${tieCounter}`;
+    totalDiv.innerText = `Round: ${gameCounter}`;
+    checkScore();
+});
+
+const paperButton = document.getElementById('paper');
+paperButton.addEventListener('click', e => {
+    playRound(e.target.value, computerPlay());
+    winsDiv.innerText = `Wins: ${winCounter}`;
+    lossDiv.innerText = `Losses: ${loseCounter}`;
+    tiesDiv.innerText = `Ties: ${tieCounter}`;
+    totalDiv.innerText = `Round: ${gameCounter}`;
+    checkScore();
+});
+
+const scissorsButton = document.getElementById('scissors');
+scissorsButton.addEventListener('click', e => {
+    playRound(e.target.value, computerPlay());
+    winsDiv.innerText = `Wins: ${winCounter}`;
+    lossDiv.innerText = `Losses: ${loseCounter}`;
+    tiesDiv.innerText = `Ties: ${tieCounter}`;
+    totalDiv.innerText = `Round: ${gameCounter}`;
+    checkScore();
+});
+
+// function game() {
+
+//     // let gameCounter = 0;
+//     // let winCounter = 0;
+//     // let loseCounter = 0;
+//     // let tieCounter = 0;
+
+//     while (gameCounter < 5) {
+
+//         let playerSelection = playerPlay();
+//         let computerSelection = computerPlay();
+//         result = playRound(playerSelection, computerSelection);
+//         console.log(result);
+//         gameCounter += 1;
+
+//         if (result === 'win') {
+
+//             console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n You win!');
+//             winCounter += 1;
+
+//         } else if (result === 'lose') {
+
+//             console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n You lose!');
+//             loseCounter += 1;
+
+//         } else {
+//             console.log('You play: ' + playerSelection + '\n Computer plays: ' + computerSelection + '. \n Try again!');
+//             tieCounter += 1;
+//         }
+
+//     };
+
+//     console.log('Wins: ' + winCounter);
+//     console.log('Losses: ' + loseCounter);
+//     console.log('Ties: ' + tieCounter)
+
+//     if (winCounter > loseCounter) {
+//         console.log('You win!')
+//     } else if (winCounter < loseCounter) {
+//         console.log('You Lose!')
+//     } else {
+//         console.log('It\'s a tie!')
+//     }
+
+// }
